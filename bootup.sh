eade782dc023eb786eb16e0d66fa4a29caa61356
Viresh Kumar <viresh.kumar@linaro.org>
2016-03-04 21:40:31 +0530

    macupdates
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- bootup.sh
+++ bootup.sh
@@ -6,6 +6,6 @@ export SUDO_ASKPASS=~/scripts/pswd.sh
 
 sudo -A service minidlna restart
 sudo -A service minidlna force-reload
-fetchmail -d 60 -L ~/.fetchmaillog
+#fetchmail -d 60 -L ~/.fetchmaillog
 #sudo -A ethtool -s enp0s31f6 wol g
-/usr/lib/vino/vino-server
+#/usr/lib/vino/vino-server

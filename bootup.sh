7f16626e8ce3a11192583168102e13b163a0478a
Viresh Kumar <viresh.kumar@linaro.org>
2017-10-04 07:38:17 +0530

    Revert "disable-fetchmail"
    This reverts commit e924bdb26e60e1c3f20a32b4345c4fceac53f43c.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- bootup.sh
+++ bootup.sh
@@ -6,7 +6,7 @@ export SUDO_ASKPASS=~/scripts/pswd.sh
 
 sudo -A service minidlna restart
 sudo -A service minidlna force-reload
-# fetchmail -d 60 -L ~/.fetchmaillog
+fetchmail -d 60 -L ~/.fetchmaillog
 #sudo -A ethtool -s enp0s31f6 wol g
 /usr/lib/vino/vino-server
 mounthikey
